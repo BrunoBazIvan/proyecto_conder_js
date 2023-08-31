e2f5f152918c8bd42648c26e3a0d17d995deabef
Bruno Baz <bazbruno13@gmail.com>
2023-08-31 14:05:03 -0300

    implemente la funcion de busqueda

---
+++ js/loguin.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    const btn = document.getElementById("login");
+
+    btn.addEventListener("click", function () {
+// se obtiene el valor ingresado en el por el usuario de nombre.
+    const user = document.getElementById("nombre").value;
+
+// se obtiene el valor ingresado en el por el usuario de contraseña.
+    const pass = document.getElementById("contraseña").value;
+
+// se obtiene la informacion para saber si el usuario activo el checkbox.
+    const recordar = document.querySelector("input[type='checkbox']").checked;
+
+//La función validarpass() se define para verificar si la longitud del nombre de usuario es mayor que 4 y la longitud de la contraseña es mayor que 6. Si ambas condiciones se cumplen, la función devuelve true, de lo contrario, devuelve false.
+    function validarPassword (){
+        if (user.length > 4 && pass.length > 6){
+            return true;
+        }else{
+            return false;
+        }    
+    }
+
+     if(validarPassword()){
+            if(recordar){
+                localStorage.setItem("user", user);
+            }else{
+                sessionStorage.setItem("user", user)
+            }
+            window.location.href = "index.html";
+        }else {
+            alert("Datos incorrectos");
+          }
+    });
+});
--- js/main.js
+++ js/main.js
@@ -7,42 +7,68 @@ const productos = [
     {nombre:"Dados Zelda", precio:600},
 ]
 
-let carrito = [];
-let nombreDados = "";
+// let carrito = [];
+// let nombreDados = "";
 
-for (const nombres of productos) {
-   nombreDados += nombres.nombre + "\n" ;
-}
+// for (const nombres of productos) {
+//    nombreDados += nombres.nombre + "\n" ;
+// }
 
-let elegirMasDados = true;
+// let elegirMasDados = true;
 
-while(elegirMasDados){
-    let elegirDado = prompt("Nombres de los dados disponibles: \n" + nombreDados + "terminar")
+// while(elegirMasDados){
+//     let elegirDado = prompt("Nombres de los dados disponibles: \n" + nombreDados + "terminar")
 
-    if (elegirDado.toLowerCase() === 'terminar') {
-        elegirMasDados = false;
-        }else {
-            let productoEncontrado = productos.find(producto => producto.nombre === elegirDado);
+//     if (elegirDado.toLowerCase() === 'terminar') {
+//         elegirMasDados = false;
+//         }else {
+//             let productoEncontrado = productos.find(producto => producto.nombre === elegirDado);
         
 
-        if (productoEncontrado) {
-            carrito.push(productoEncontrado);
-        alert(`Dado ${elegirDado} agregado al carrito.`);
-    }else{
-        alert("Nombre de dado no válido. Por favor, elige un nombre válido o escribe 'terminar' para finalizar.")
-        }
+//         if (productoEncontrado) {
+//             carrito.push(productoEncontrado);
+//         alert(`Dado ${elegirDado} agregado al carrito.`);
+//     }else{
+//         alert("Nombre de dado no válido. Por favor, elige un nombre válido o escribe 'terminar' para finalizar.")
+//         }
+//     }
+// }
+
+// let total = 0;
+
+// let carritoTexto = "Has terminado de agregar dados al carrito. Aquí está tu carrito:\n";
+
+// for (const producto of carrito) {
+//     carritoTexto += `${producto.nombre} Precio: ${producto.precio}\n`;
+//     total += producto.precio;
+// }
+
+// carritoTexto += `Total: ${total}`;
+
+// alert(carritoTexto)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const isSessionRecordada = localStorage.getItem("user");
+    const isSessionActive = sessionStorage.getItem("user");
+  
+    // Redirigir según la condición
+    if (!isSessionActive) {
+      window.location.href = 'login.html';
     }
-}
+  });
 
-let total = 0;
+  const articulos = document.querySelectorAll(".articulo");
 
-let carritoTexto = "Has terminado de agregar dados al carrito. Aquí está tu carrito:\n";
-
-for (const producto of carrito) {
-    carritoTexto += `${producto.nombre} Precio: ${producto.precio}\n`;
-    total += producto.precio;
-}
-
-carritoTexto += `Total: ${total}`;
-
-alert(carritoTexto)
+  document.addEventListener("keyup", e => {
+      if (e.target.matches("#buscador")) {
+          const searchTerm = e.target.value.trim().toLowerCase();
+          articulos.forEach(item => {
+              const itemName = item.querySelector(".card-title").textContent.toLowerCase();
+              if (itemName.includes(searchTerm)) {
+                  item.style.display = "block";
+              } else {
+                  item.style.display = "none";
+              }
+          });
+      }
+  });
